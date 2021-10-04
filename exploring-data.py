bb5f4a65f982c04b875e4cfbcf08e14ca2f3962e
Tristan-Prevost <91889549+Tristan-Prevost@users.noreply.github.com>
2021-10-04 15:14:34 +0200

    Typo fixed

--- exploring-data.py
+++ exploring-data.py
@@ -9,7 +9,7 @@ from google.colab import drive
 drive.mount('/content/drive')
 
 # returns JSON object as a dictionary
-f = open('/content/drive/MyDrive/Iceberg/train.json',) # The train.json file needs to be in an "Icerberg" directory
+f = open('/content/drive/MyDrive/Iceberg/train.json',) # The train.json file needs to be in an "Iceberg" directory
 
 # creating a dataframe
 df = pd.DataFrame(json.load(f))
