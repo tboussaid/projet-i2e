77a62be5706af3297c824ac559311db4583101b8
tboussaid <taha.boussaid@insa-lyon.fr>
2021-10-04 14:32:47 +0200

    Create exploring-data.py

---
+++ exploring-data.py
@@ -0,0 +1,14 @@
+# First file for accessing the training dataset
+
+# Import useful labraries
+import pandas as pd
+import json
+
+# returns JSON object as a dictionary
+f = open('train.json',)
+
+# creating a dataframe
+df = pd.DataFrame(json.load(f))
+
+# visualising the first rows
+print(df.head())
