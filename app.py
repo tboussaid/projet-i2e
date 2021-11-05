09ac8a4dd52b3f83f6f5ea70fbfa665cf6d66ea6
Tristan-Prevost <91889549+Tristan-Prevost@users.noreply.github.com>
2021-11-05 21:27:08 +0100

    Nouvelle classe EvaluateAndReport

---
+++ app.py
@@ -0,0 +1,180 @@
+# Import useful libraries
+import pandas as pd
+import json
+import numpy as np
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale, normalize, StandardScaler
+
+# Classifier models
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, cross_val_predict, GridSearchCV 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_absolute_error
+
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from sklearn.metrics import precision_recall_curve
+
+def show_bands(row):
+  print(f'Index : {row.name}')
+  f, (img1, img2) = plt.subplots(1, 2, figsize = (10,10))
+  img1.imshow(np.array(row.band_1).reshape(75,75))
+  img1.set_title("band_1")
+  img2.imshow(np.array(row.band_2).reshape(75,75))
+  img2.set_title("band_2")
+  img1.set_yticks([])
+  img1.set_xticks([])
+  img2.set_yticks([])
+  img2.set_xticks([])
+  plt.show()
+
+class EvaluateAndReport:
+  def __init__(self, df, X_train, X_test, y_train, y_test, scoring = 'f1', grid_cv = 5, cv_cv = 10, threshold = 0.5):
+    self.df = df
+    self.X_train, self.y_train, self.X_test, self.y_test = X_train, y_train, X_test, y_test
+    self.scoring = scoring
+    self.grid_cv = grid_cv
+    self.cv_cv = cv_cv
+    self.threshold = threshold
+
+
+  def grid_report(self, classifier = None, param_grid = None):
+    if classifier is None:
+      classifier = self.last_best
+    if not(param_grid is None):
+      print('--- Grid Search Report ---')
+      grid_search = GridSearchCV(classifier, param_grid, cv = self.grid_cv, scoring = self.scoring, return_train_score = True)
+      grid_search.fit(self.X_train, self.y_train)
+
+      print(f'Best parameters for {classifier} :\n {grid_search.best_params_} with {self.scoring} score {round(grid_search.best_score_,3)}')
+
+      self.last_best = grid_search.best_estimator_
+    else:
+      print("cross_report should be used when there is no param_grid")
+
+
+  def cross_report(self, classifier = None):
+    if classifier is None:
+      classifier = self.last_best
+    print('--- Cross Validation Report ---')
+    scorings = ('accuracy', 'f1', 'precision', 'recall')
+    scores = cross_validate(classifier, X_train, y_train, cv = self.cv_cv, scoring = scorings)
+    [print(f'Mean {scoring} score {round(np.mean(scores["test_"+scoring]),3)} with std {round(np.std(scores["test_"+scoring]),3)}') for scoring in scorings]
+
+
+  def test_report(self, classifier = None):
+    if classifier is None:
+      classifier = self.last_best
+    print('--- Test Sample Report ---')
+    classifier.fit(self.X_train, self.y_train)
+    self.last_y_pred = classifier.predict(self.X_test)
+
+    print(f'Scores for the test sample :')
+    print(f' accuracy : {round(accuracy_score(self.y_test, self.last_y_pred),3)}')
+    print(f' f1 : {round(f1_score(self.y_test, self.last_y_pred),3)}')
+    print(f' precision : {round(precision_score(self.y_test, self.last_y_pred),3)}')
+    print(f' recall : {round(recall_score(self.y_test, self.last_y_pred),3)}')
+
+    print('Confusion matrix for the test sample :')
+    cm = confusion_matrix(self.y_test, self.last_y_pred)
+    ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Boat','Iceberg']).plot(cmap="Blues", values_format='')
+
+
+  def grid_cross_test_report(self, classifier = None, param_grid = None):
+    if classifier is None:
+      classifier = self.last_best
+    if not(param_grid is None):
+      self.grid_report(classifier, param_grid)
+      self.cross_report(self.last_best)
+      self.test_report(self.last_best)
+    else:
+      print('cross_test_report should be used when there is no param_grid')
+
+  def cross_test_report(self, classifier = None):
+    if classifier is None:
+      classifier = self.last_best
+    self.cross_report(classifier)
+    self.test_report(classifier)
+
+  def cross_proba_report(self, classifier = None, threshold = 0.5):
+    if classifier is None:
+      classifier = self.last_best
+    print(f'--- Cross Validation with {threshold} Threshold Report ---')
+    y_train_scores = cross_val_predict(self.last_best, self.X_train, self.y_train, cv=self.cv_cv, method='predict_proba')
+    y_train_scores = np.array([y[1] for y in y_train_scores])
+    y_pred = (y_train_scores >= threshold)
+
+    print(f' accuracy : {round(accuracy_score(self.y_train, y_pred),3)}')
+    print(f' f1 : {round(f1_score(self.y_train, y_pred),3)}')
+    print(f' precision : {round(precision_score(self.y_train, y_pred),3)}')
+    print(f' recall : {round(recall_score(self.y_train, y_pred),3)}')
+
+  def plot_precision_recall(self, classifier = None):
+    if classifier is None:
+      classifier = self.last_best
+    print('--- Precision Recall Curves ---')
+    y_train_scores = cross_val_predict(self.last_best, self.X_train, self.y_train, cv=self.cv_cv, method='predict_proba')
+    y_train_scores = np.array([y[1] for y in y_train_scores])
+    precisions, recalls, probas = precision_recall_curve(self.y_train, y_train_scores)
+
+    plt.figure(0)
+    plt.plot(probas, precisions[:-1], "b--", label="Precision", linewidth=2)
+    plt.plot(probas, recalls[:-1], "g-", label="Recall", linewidth=2)
+    plt.xlabel("Probablility threshold", fontsize=16)
+    plt.ylabel("Score", fontsize=16)
+    plt.legend(loc="best", fontsize=16)
+    plt.ylim([0, 1])
+
+    plt.figure(1)
+    plt.plot(recalls, precisions, "b-", linewidth=2)
+    plt.xlabel("Recall", fontsize=16)
+    plt.ylabel("Precision", fontsize=16)
+    plt.axis([0, 1, 0, 1])
+
+  def plot_mistakes(self, nb_samples = 1, nature = None, random_state = None):
+    #Plots bands for wrongly predicted individuals
+    #By default, false positives and false negatives are shown. If one is chosen in 'nature', the other ones won't be shown
+    df_res = df.join(pd.DataFrame(data=self.last_y_pred, index=self.y_test.index, columns=['is_iceberg_pred']))
+    
+    if nature != 'boat_as_iceberg':
+      print('--- Icebergs predicted as boats ---')
+      df_res[(df_res['is_iceberg_pred'] == 0) & df_res['is_iceberg'] == 1].sample(nb_samples, random_state = random_state).apply(show_bands, axis=1)
+    if nature != 'iceberg_as_boat':
+      print('--- Boats predicted as Icebergs ---')
+      df_res[(df_res['is_iceberg_pred'] == 1) & df_res['is_iceberg'] == 0].sample(nb_samples, random_state = random_state).apply(show_bands, axis=1)
+
+  def plot_predicted(self, nb_samples = 1, nature = None, random_state = None):
+    #Plots bands for correclty predicted individuals
+    #By default, correctly predicted icebergs and boats are shown. If one is chosen in 'nature', the others ones won't be shown
+    df_res = df.join(pd.DataFrame(data=self.last_y_pred, index=self.y_test.index, columns=['is_iceberg_pred']))
+    
+    if nature != 'boats':
+      print('--- Icebergs predicted correctly ---')
+      df_res[(df_res['is_iceberg_pred'] == 1) & df_res['is_iceberg'] == 1].sample(nb_samples, random_state = random_state).apply(show_bands, axis=1)
+    if nature != 'icebergs':
+      print('--- Boats predicted correctly ---')
+      df_res[(df_res['is_iceberg_pred'] == 0) & df_res['is_iceberg'] == 0].sample(nb_samples, random_state = random_state).apply(show_bands, axis=1)
+
+
+# Adding more features to the initial dataset
+def add_features(data, label) :
+  data['max_b'+str(label)] = [np.max(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['min_b'+str(label)] = [np.min(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['median_b'+str(label)] = [np.median(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['mean_b'+str(label)] = [np.mean(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['argmax_b'+str(label)] = [np.argmax(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['argmin_b'+str(label)] = [np.argmin(np.array(matx)) for matx in data['band_'+str(label)]]
+  data['band_'+str(label)] = [np.array(matx).reshape(75,75) for matx in data['band_'+str(label)]]
+  return data
+
+# Plotting the new feature in a histogram
+def plot_features(data, name):
+  plt.hist(data.loc[data.is_iceberg==1,name], bins=50, color='navy',alpha=0.5,label='Iceberg')
+  plt.hist(data.loc[data.is_iceberg==0,name], bins=50,color='firebrick',alpha=0.5, label='Ship')
+  plt.legend()
+  plt.xlabel(name)
+  plt.ylabel('Frequency')
+  plt.show()
